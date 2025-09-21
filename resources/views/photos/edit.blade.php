b8c2c0b7e352561b80d92309baf204965dcb6b93
YK4RU <ana.lima1801@aluno.ce.gov.br>
2025-09-21 12:45:17 -0300

    refact: mudança para o method PUT para evitar má prática

--- resources/views/photos/edit.blade.php
+++ resources/views/photos/edit.blade.php
@@ -1,18 +1,18 @@
-<h1>Cadastro</h1>
+<h1>EDIT</h1>
 
 @if (session()->has('message'))
     {{ session()->get('message') }}
 @endif
 
-<form action="{{ route('photos.update', ['photo' => $photo->id]) }}" method="post" enctype="multipart/form-data">
+<form action="{{ route('photos.update', ['photo' => $photos->id]) }}" method="post" enctype="multipart/form-data">
     @csrf
-    <input type="hidden" name="_method" value="PUT">
+    @method('PUT')
     
     <p>
         <input type="file" name="UrldaImagem" id="UrldaImagem" accept="image/*">
     </p>
     <p>
-        <input type="text" name="NomedaImagem" id="NomedaImagem" value="{{ $photo->NomedaImagem }}">
+        <input type="text" name="NomedaImagem" id="NomedaImagem" value="{{ $photos->NomedaImagem }}">
     </p>
     <p>
         <input type="submit" value="Atualizar" id="botaoAtualizar">
